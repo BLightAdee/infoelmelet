a955306295f573c3bbb6d6e5c6fad5fc95143123
BLightAdee <vassadamka2015@gmail.com>
2025-11-04 16:07:45 +0200

    Update karakterlanc.cpp
    added madarnyelv()

--- karakterlanc.cpp
+++ karakterlanc.cpp
@@ -3,6 +3,31 @@
 #include <cstring>
 using namespace std;
 
+void madarnyelv(){
+    char s[100]="kicsi kutya tarka",sz[100],*p;
+    int h=0;
+    while(s[h]!='\0'){
+      h++; //hany karakteres a lanc
+    }
+    for(int i=0;i<=h;i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            for(int j=h;j>=i;j--){
+                s[j+2]=s[j];
+            }
+            s[i+1]='p';
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                s[i+2]=s[i];
+            }
+            else{
+                s[i+2]=s[i]+'A'-'a';
+            }
+            h=h+2;
+            i+=2;
+        }
+    }
+    cout<<s<<"F";
+}
+
 void spaceTorles(){
     char s[100]="  kicsi    kutya   tarka  ",sz[100],*p;
     while(s[0]==' '){
